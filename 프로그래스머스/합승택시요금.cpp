45e7c0c8870972d641c5aaba2c1304413040178d
Lee Jin Gi <dlwlsrl99@gmail.com>
2023-08-17 22:54:57 +0900

    프로그래머스 합승택시 플로이드 와샬

---
+++ 프로그래스머스/합승택시요금.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    //A와 B가 같이갈 수 있는 최단 거리, A에서 목적지까지 갈 수 있는 최단 거리, B에서 목적지까지 갈 수 있는 최단거리, AB에서 목적지까지 가는 최단거리
+    // => 목적지에서 A, 목적지에서 B, 목적지에서 AB
+    
+    int answer = 0;
+    return answer;
+}
