044c25ea5c1cdf598cc4509e8c82e70516ea3690
Lee Jin Gi <dlwlsrl99@gmail.com>
2023-08-30 20:56:18 +0900

    algorithm 정리 and 동빈나 문제

--- 동빈나문제풀이/문자열압축.cpp
+++ 동빈나문제풀이/문자열압축.cpp
@@ -4,23 +4,27 @@
 #include <iostream>
 using namespace std;
 
-int zip(int length, string data){
-    int index = 0;
-    int dup = 1;
-    string output;
-    while(index+length<data.length()){
-        if(data.substr(index,length)==data.substr(index+length,length)){
-            index+=length;
+int zip(int length, string text){
+    //aaaabbabbabb => 문자열을 3개로 자르는게 가장 단축화 한다.
+    string output="";
+    string pretext = text.substr(0,length);
+    string curtext;
+    int dup=1;
+    for(int i=length;i<text.size();i+=length){
+        curtext = text.substr(i,length);
+        if(pretext.compare(curtext)==0)
+            //문자열을 압축할 수 있다면.
             dup++;
-        }else{
-            if(dup>1){
-              output += to_string(dup)+data.substr(index,length);
-            }
-            dup=0;
-            index+=length;
+        else{
+            if(dup>1) output+= to_string(dup);
+            output+= pretext;
+            pretext = curtext;
+            dup=1;
         }
         
     }
+    if(dup>1) output+= to_string(dup);
+    output += pretext;
     cout << output << endl;
     return output.size();
 }
